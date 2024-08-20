0a7f33b6fb90ff0ad90200149701626e60caa54a
anshulagrawal07 <75289464+anshulagrawal07@users.noreply.github.com>
2024-08-20 12:50:03 +0000

    Change for Anshul Test Repo

--- steps/01_setup_snowflake.sql
+++ steps/01_setup_snowflake.sql
@@ -1,26 +1,26 @@
 USE ROLE ACCOUNTADMIN;
 
-CREATE WAREHOUSE IF NOT EXISTS QUICKSTART_WH WAREHOUSE_SIZE = XSMALL, AUTO_SUSPEND = 300, AUTO_RESUME= TRUE;
+CREATE WAREHOUSE IF NOT EXISTS GIT_WH WAREHOUSE_SIZE = XSMALL, AUTO_SUSPEND = 300, AUTO_RESUME= TRUE;
 
 
 -- Separate database for git repository
-CREATE DATABASE IF NOT EXISTS QUICKSTART_COMMON;
+CREATE DATABASE IF NOT EXISTS git_int_test;
 
 
 -- API integration is needed for GitHub integration
 CREATE OR REPLACE API INTEGRATION git_api_integration
   API_PROVIDER = git_https_api
-  API_ALLOWED_PREFIXES = ('https://github.com/<insert GitHub username>') -- INSERT YOUR GITHUB USERNAME HERE
+  API_ALLOWED_PREFIXES = ('https://github.com/anshulagrawal07') -- INSERT YOUR GITHUB USERNAME HERE
   ENABLED = TRUE;
 
 
 -- Git repository object is similar to external stage
-CREATE OR REPLACE GIT REPOSITORY quickstart_common.public.quickstart_repo
+CREATE OR REPLACE GIT REPOSITORY git_int_test.public.git_repo
   API_INTEGRATION = git_api_integration
-  ORIGIN = '<insert URL of forked GitHub repo>'; -- INSERT URL OF FORKED REPO HERE
+  ORIGIN = 'https://github.com/anshulagrawal07/sfguide-getting-started-with-snowflake-devops'; -- INSERT URL OF FORKED REPO HERE
 
 
-CREATE OR REPLACE DATABASE QUICKSTART_PROD;
+CREATE OR REPLACE DATABASE git_int_prod;
 
 
 -- To monitor data pipeline's completion
@@ -41,4 +41,4 @@ CREATE OR REPLACE STAGE bronze.raw;
 
 
 -- Copy file from GitHub to internal stage
-copy files into @bronze.raw from @quickstart_common.public.quickstart_repo/branches/main/data/airport_list.json;
+copy files into @bronze.raw from @git_int_test.public.git_repo/branches/main/data/airport_list.json;
